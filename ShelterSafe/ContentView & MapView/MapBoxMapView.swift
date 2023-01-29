28f83a3d969cec37ec45bd60097b23802749558b
Srisaivikas Munagala <srisaivikas.064@gmail.com>
2023-01-29 02:05:29 -0600

    Changed Pitch to 45

--- ShelterSafe/ContentView & MapView/MapBoxMapView.swift
+++ ShelterSafe/ContentView & MapView/MapBoxMapView.swift
@@ -27,7 +27,7 @@ class MapViewController: UIViewController {
    override public func viewDidLoad() {
        super.viewDidLoad()
        let myResourceOptions = ResourceOptions(accessToken: "pk.eyJ1Ijoib25pa2giLCJhIjoiY2xiMWtyNG5kMDR1bTN3b2Z6NGtmbm92bSJ9.jktBy9muy0FjQvjshVeORg")
-       let myCameraOptions = CameraOptions(center: CLLocationCoordinate2D(latitude: 33.123806, longitude: -96.67585), zoom: 16, pitch: 30)
+       let myCameraOptions = CameraOptions(center: CLLocationCoordinate2D(latitude: 33.123806, longitude: -96.67585), zoom: 16, pitch: 45)
        let myMapInitOptions = MapInitOptions(resourceOptions: myResourceOptions, cameraOptions: myCameraOptions)
        mapView = MapView(frame: view.bounds, mapInitOptions: myMapInitOptions)
        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
