37fbf31f1f1e0384e78eb16151f30a667c2b83b9
jishnud17 <72703096+jishnud17@users.noreply.github.com>
2023-02-17 17:13:21 -0600

    Revert "Merge branch 'main' of https://github.com/CSAllenISD/2023-ISP-ShelterSafe"
    This reverts commit bf7033d9d8e27926a872bbc81ae90da8603c82cc, reversing
    changes made to 5bf77b3d25b633de0ebc493fe88094ebf2579ab6.

--- ShelterSafe/ContentView & MapView/MapBoxMapView.swift
+++ ShelterSafe/ContentView & MapView/MapBoxMapView.swift
@@ -5,70 +5,65 @@
 //  Created by Onik Houqe on 11/29/22.
 //
 
-import SwiftUI
-import MapboxCommon
-import MapboxMaps
-import MapboxCoreMaps
 
+import SwiftUI
+import UIKit
+//import MapboxCommon
+//import MapboxMaps
+//import MapboxCoreMaps
+import CoreLocation
+import MapKit
+
+
+
+/*let locationManager = CLLocationManager()
 struct MapBoxMapView: UIViewControllerRepresentable {
-     
-    func makeUIViewController(context: Context) -> ViewController {
-           return ViewController()
+    
+    func makeUIViewController(context: Context) -> MapViewController {
+           return MapViewController()
        }
       
-    func updateUIViewController(_ uiViewController: ViewController, context: Context) {
+    func updateUIViewController(_ uiViewController: MapViewController, context: Context) {
         
+
     }
+    locationManager.delegate = self
+
 }
 
-class ViewController: UIViewController {
+class MapViewController: UIViewController, CLLocationManagerDelegate, LocationPermissionsDelegate {
    internal var mapView: MapView!
    override public func viewDidLoad() {
        super.viewDidLoad()
-       
-       
-       
        let myResourceOptions = ResourceOptions(accessToken: "pk.eyJ1Ijoib25pa2giLCJhIjoiY2xiMWtyNG5kMDR1bTN3b2Z6NGtmbm92bSJ9.jktBy9muy0FjQvjshVeORg")
-       //let myCameraOptions = CameraOptions(center: CLLocationCoordinate2D(latitude: 33.123806, longitude: -96.67585), zoom: 16, pitch: 30)
-       //let myMapInitOptions = MapInitOptions(resourceOptions: myResourceOptions, cameraOptions: myCameraOptions)
-       
-       let myMapInitOptions = MapInitOptions(resourceOptions: myResourceOptions)
-       
-       
+       let myCameraOptions = CameraOptions(center: CLLocationCoordinate2D(latitude: 33.1003966, longitude: -96.6586053), zoom: 16, pitch: 45)
+       let myMapInitOptions = MapInitOptions(resourceOptions: myResourceOptions, cameraOptions: myCameraOptions)
        mapView = MapView(frame: view.bounds, mapInitOptions: myMapInitOptions)
-       
-       mapView.location.delegate = self
-       mapView.location.options.puckType = .puck2D()
-       
-
-       
-       
-       
-       
        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
        self.view.addSubview(mapView)
+       
+       mapView.location.delegate = self
+       mapView.location.options.activityType = .other
+       mapView.location.options.puckType = .puck2D()
+       mapView.location.locationProvider.startUpdatingLocation()
+
+
+       
       // var pointAnnotation = PointAnnotation(coordinate: CLLocationCoordinate2D(latitude: 33.123806, longitude: -96.67585))
+           }
+       
    }
-    
-    
-    func requestPermissionsButtonTapped() {
-            mapView.location.requestTemporaryFullAccuracyPermissions(withPurposeKey: "CustomKey")
-        }
-    
-    
-}
 
-
-extension ViewController: LocationPermissionsDelegate {
-    
-    func locationManager(_ locationManager: LocationManager, didChangeAccuracyAuthorization accuracyAuthorization: CLAccuracyAuthorization) {
-        if accuracyAuthorization == .reducedAccuracy {
-         // Perform an action in response to the new change in accuracy
-        }
+extension MapViewController {
+    func locationUpdate(newLocation: CLLocation) {
+        mapView.camera.fly(to: CameraOptions(center: newLocation.coordinate, zoom: 16), duration: 5.0)
+        print(newLocation.coordinate)
     }
-    
-    
 }
 
+    /*func locationUpdate() {
+        mapView.camera.fly(to: CameraOptions(center: CLLocationCoordinate2D(latitude: 33.123806, longitude: -96.67585), zoom: 16), duration: 5.0)
+    }*/
+    */
 
 
