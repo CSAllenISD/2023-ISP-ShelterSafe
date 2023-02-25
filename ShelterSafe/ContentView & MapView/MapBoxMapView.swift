e0fdd0e3eaeb7c1fdcde199c6ec67b83dc5bb556
jishnud17 <72703096+jishnud17@users.noreply.github.com>
2023-02-24 18:11:44 -0600

    Revert "Merge branch 'pranavCommits'"
    This reverts commit f8ebf722e92335d3d4467040e9b1ee773ff43b14, reversing
    changes made to c5a1c10b34aaa87c389bbde4bb61a4b52a2872aa.

--- ShelterSafe/ContentView & MapView/MapBoxMapView.swift
+++ ShelterSafe/ContentView & MapView/MapBoxMapView.swift
@@ -6,7 +6,6 @@
 //
 
 import SwiftUI
-import UIKit
 import MapboxCommon
 import MapboxMaps
 import MapboxCoreMaps
@@ -23,16 +22,8 @@ struct MapBoxMapView: UIViewControllerRepresentable {
     }
 }
 
-<<<<<<< HEAD
 class ViewController: UIViewController, CLLocationManagerDelegate {
    private var mapView: MapView!
-=======
-
-class ViewController: UIViewController {
-   internal var mapView: MapView!
-    
-    
->>>>>>> pranavCommits
    override public func viewDidLoad() {
        super.viewDidLoad()
        
@@ -49,19 +40,9 @@ class ViewController: UIViewController {
        }
        
        let myResourceOptions = ResourceOptions(accessToken: "pk.eyJ1Ijoib25pa2giLCJhIjoiY2xiMWtyNG5kMDR1bTN3b2Z6NGtmbm92bSJ9.jktBy9muy0FjQvjshVeORg")
-<<<<<<< HEAD
        
        //let myCameraOptions = CameraOptions(center: locationManager.location?.coordinate, zoom: 16, pitch: 45)
        let myMapInitOptions = MapInitOptions(resourceOptions: myResourceOptions)
-=======
-       let myCameraOptions = CameraOptions(center: CLLocationCoordinate2D(latitude: 30.123806, longitude: -96.67585), zoom: 6, pitch: 30)
-       //let myMapInitOptions = MapInitOptions(resourceOptions: myResourceOptions, cameraOptions: myCameraOptions)
-       
-     
-       
-       
-       let myMapInitOptions = MapInitOptions(resourceOptions: myResourceOptions, cameraOptions: myCameraOptions)
->>>>>>> pranavCommits
        
        //let myMapInitOptions = MapInitOptions(resourceOptions: myResourceOptions, cameraOptions: myCameraOptions)
        
@@ -78,32 +59,11 @@ class ViewController: UIViewController {
        
        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
        self.view.addSubview(mapView)
-<<<<<<< HEAD
       // var pointAnnotation = PointAnnotation(coordinate: CLLocationCoordinate2D(latitude: 33.123806, longitude: -96.67585))
        mapView.mapboxMap.onNext(event: .mapLoaded) { _ in
            
            self.mapView.camera.fly(to: CameraOptions(center: locationManager.location?.coordinate, zoom: 16, pitch: 45), duration: 2.0)
        }
-=======
-       
-       
-       //POINT ANNOTATIONS
-       var pointAnnotation = PointAnnotation(coordinate: CLLocationCoordinate2D(latitude: 33.123806, longitude: -96.67585))
-       
-       pointAnnotation.image = .init(image: UIImage(named: "Image.png")!, name: "Image.png")
-       
-       let pointAnnotationManager = mapView.annotations.makePointAnnotationManager()
-       pointAnnotationManager.annotations = [pointAnnotation]
-       
-       
-       
-       if let locationCoordinate = self.mapView?.location.latestLocation?.coordinate {
-           mapView.mapboxMap.setCamera(to: CameraOptions(center: locationCoordinate, zoom: 15))
-           print("TEST")
-       }
-       print("TEST2")
-       
->>>>>>> pranavCommits
    }
     
     
