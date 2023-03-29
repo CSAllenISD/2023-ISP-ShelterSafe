5ed7ffac2cb11286ab9f266b966bd96cdfd6149f
PranavKrishnan05 <Pranav.Krishnan05@gmail.com>
2023-03-29 10:39:18 -0500

    allowed  to receive tap events on annotations conforming to AnnotationInteractionDelegate (and setting yourself as the delegate on the manager). (don't merge with main yet)

--- ShelterSafe/ContentView & MapView/MapBoxMapView.swift
+++ ShelterSafe/ContentView & MapView/MapBoxMapView.swift
@@ -53,21 +53,27 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
        mapView.location.delegate = self
        mapView.location.options.puckType = .puck2D()
        
+      
        
        
+       
+
+     
        
        
        
        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
        self.view.addSubview(mapView)
-       
-       var pointAnnotation = PointAnnotation(coordinate: CLLocationCoordinate2D(latitude: 33.123806, longitude: -96.67585))
+       let coordinate = CLLocationCoordinate2DMake(32.7767, -96.7970)
+       var pointAnnotation = PointAnnotation(coordinate: coordinate)
+               pointAnnotation.image = .init(image: UIImage(named: "red_pin")!, name: "red_pin")
        mapView.mapboxMap.onNext(event: .mapLoaded) { _ in
            
            self.mapView.camera.fly(to: CameraOptions(center: locationManager.location?.coordinate, zoom: 16, pitch: 45), duration: 2.0)
            
-           // Make the annotation show a red pin
-           pointAnnotation.image = .init(image: UIImage(named: "red_pin")!, name: "red_pin")
+       
+       
+       // Make the annotation show a red pin
            pointAnnotation.iconAnchor = .bottom
            
            // Create the `PointAnnotationManager` which will be responsible for handling this annotation
@@ -85,7 +91,11 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     
     
 }
-
+extension ViewController: AnnotationInteractionDelegate {
+    public func annotationManager(_ manager: AnnotationManager, didDetectTappedAnnotations annotations: [Annotation]) {
+        print("Annotations tapped: \(annotations)")
+    }
+}
 
 extension ViewController: LocationPermissionsDelegate {
     
