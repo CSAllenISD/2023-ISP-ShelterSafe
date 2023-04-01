655cc8bc1a410319f20d2da45aeda39b4031fc2d
PranavKrishnan05 <Pranav.Krishnan05@gmail.com>
2023-03-31 23:35:31 -0500

    Fixed Bugs in mapbox api annotation delegate

---
+++ ShelterSafe/Data/RawData.swift
@@ -0,0 +1,132 @@
+//
+//  RawData.swift
+//  ShelterSafe
+//
+//  Created by Onik Hoque on 3/15/23.
+//
+
+import Foundation
+import UIKit
+import SwiftUI
+import CoreLocation
+import CoreLocationUI
+
+//u can make similar functions to "getUsers" for different apis using different model files. this is the controller file
+
+class RawData {
+    
+    @EnvironmentObject var locationManager : LocationManager
+    
+    var userLatitude: String { return "\(locationManager.lastLocation?.coordinate.latitude ?? 0)" }
+    var userLongitude: String { return "\(locationManager.lastLocation?.coordinate.longitude ?? 0)" }
+    
+    
+    
+
+    
+    
+    func getLink() -> String {
+        return "https://api.weather.gov/alerts/active?point=\(userLatitude),\(userLongitude)"
+    }
+    
+    
+    
+    func getUsers(completion:@escaping ([User]) -> ()) {
+            guard let url = URL(string: "https://jsonplaceholder.typicode.com/users") else { return }
+        
+            URLSession.shared.dataTask(with: url) { (data, _, _) in
+                let users = try! JSONDecoder().decode([User].self, from: data!)
+                print(users)
+                
+                DispatchQueue.main.async {
+                    completion(users)
+                }
+            }
+            .resume()
+        }
+    
+  /*  func getAlerts(completion:@escaping ([NWSAlertFeature]) -> ()) {
+            guard let url = URL(string: "https://api.weather.gov/alerts/active?area=NY") else { return }
+        
+            URLSession.shared.dataTask(with: url) { (data, _, _) in
+                let alert = try! JSONDecoder().decode(NWSAlert.self, from: data!)
+                let features = alert.features
+        
+                
+                DispatchQueue.main.async {
+                    completion(features)
+                }
+            }
+            .resume()
+        } */
+    
+    func getAlerts(completion:@escaping (NWSAlert) -> ()) {
+            guard let url = URL(string: "https://api.weather.gov/alerts/active?area=WV") else { return }
+        
+            URLSession.shared.dataTask(with: url) { (data, _, _) in
+                let alert = try! JSONDecoder().decode(NWSAlert.self, from: data!)
+               // let features = alert.features
+        
+                
+                DispatchQueue.main.async {
+                    completion(alert)
+                }
+            }
+            .resume()
+        }
+    
+    
+    
+
+
+}
+
+
+
+
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+
+    private let locationManager = CLLocationManager()
+    @Published var locationStatus: CLAuthorizationStatus?
+    @Published var lastLocation: CLLocation?
+
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+
+   
+    
+    var statusString: String {
+        guard let status = locationStatus else {
+            return "unknown"
+        }
+        
+        switch status {
+        case .notDetermined: return "notDetermined"
+        case .authorizedWhenInUse: return "authorizedWhenInUse"
+        case .authorizedAlways: return "authorizedAlways"
+        case .restricted: return "restricted"
+        case .denied: return "denied"
+        default: return "unknown"
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        locationStatus = status
+        print(#function, statusString)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        lastLocation = location
+        print(#function, location)
+    }
+}
+
+
+
+
---
+++ ShelterSafe/MapBoxMapView.swift
@@ -0,0 +1,140 @@
+//
+//  MapBoxMapView.swift
+//  ShelterSafe
+//
+//  Created by Onik Houqe on 11/29/22.
+//
+
+import SwiftUI
+import MapboxCommon
+import MapboxMaps
+import MapboxCoreMaps
+import CoreLocation
+
+struct MapBoxMapView: UIViewControllerRepresentable {
+     
+    func makeUIViewController(context: Context) -> ViewController {
+           return ViewController()
+       }
+      
+    func updateUIViewController(_ uiViewController: ViewController, context: Context) {
+        
+    }
+}
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+   private var mapView: MapView!
+   override public func viewDidLoad() {
+       super.viewDidLoad()
+       
+       
+       let locationManager = CLLocationManager()
+       
+       // For use in foreground
+       //self.locationManager.requestWhenInUseAuthorization()
+       locationManager.requestWhenInUseAuthorization()
+
+       if CLLocationManager.locationServicesEnabled() {
+           locationManager.delegate = self
+           locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+           locationManager.startUpdatingLocation()
+       }
+       
+       let myResourceOptions = ResourceOptions(accessToken: "pk.eyJ1Ijoib25pa2giLCJhIjoiY2xiMWtyNG5kMDR1bTN3b2Z6NGtmbm92bSJ9.jktBy9muy0FjQvjshVeORg")
+       
+       //let myCameraOptions = CameraOptions(center: locationManager.location?.coordinate, zoom: 16, pitch: 45)
+       let myMapInitOptions = MapInitOptions(resourceOptions: myResourceOptions)
+       
+       //let myMapInitOptions = MapInitOptions(resourceOptions: myResourceOptions, cameraOptions: myCameraOptions)
+       
+       
+       mapView = MapView(frame: view.bounds, mapInitOptions: myMapInitOptions)
+       
+       mapView.location.delegate = self
+       mapView.location.options.puckType = .puck2D()
+       
+      
+       
+       
+       
+
+     
+       
+       
+       
+       mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+       self.view.addSubview(mapView)
+<<<<<<< HEAD:ShelterSafe/ContentView & MapView/MapBoxMapView.swift
+       let coordinate = CLLocationCoordinate2DMake(32.7767, -96.7970)
+       var pointAnnotation = PointAnnotation(coordinate: coordinate)
+               pointAnnotation.image = .init(image: UIImage(named: "red_pin")!, name: "red_pin")
+=======
+       
+<<<<<<< HEAD:ShelterSafe/ContentView & MapView/MapBoxMapView.swift
+       var pointAnnotation = PointAnnotation(coordinate: CLLocationCoordinate2D(latitude: 33.123806, longitude: -96.67585))
+>>>>>>> main:ShelterSafe/MapBoxMapView.swift
+       mapView.mapboxMap.onNext(event: .mapLoaded) { _ in
+           
+           self.mapView.camera.fly(to: CameraOptions(center: locationManager.location?.coordinate, zoom: 16, pitch: 45), duration: 2.0)
+           
+       
+       
+       // Make the annotation show a red pin
+           pointAnnotation.iconAnchor = .bottom
+           
+           // Create the `PointAnnotationManager` which will be responsible for handling this annotation
+           let pointAnnotationManager = self.mapView.annotations.makePointAnnotationManager()
+
+           // Add the annotation to the manager in order to render it on the map.
+           pointAnnotationManager.annotations = [pointAnnotation]
+=======
+       
+       //POINT ANNOTATIONS
+       let pointAnnotationArray = [PointAnnotation]()
+ 
+       
+       
+       var pointAnnotation = PointAnnotation(coordinate: CLLocationCoordinate2D(latitude: 33.123806, longitude: -96.67585))
+       
+       pointAnnotation.image = .init(image: UIImage(named: "Image.png")!, name: "Image.png")
+       
+       pointAnnotation.iconSize = 0.15
+       
+       let pointAnnotationManager = mapView.annotations.makePointAnnotationManager()
+       pointAnnotationManager.annotations = [pointAnnotation]
+       
+       
+       
+       if let locationCoordinate = self.mapView?.location.latestLocation?.coordinate {
+           mapView.mapboxMap.setCamera(to: CameraOptions(center: locationCoordinate, zoom: 15))
+           print("TEST")
+>>>>>>> onikCommits:ShelterSafe/MapBoxMapView.swift
+       }
+   }
+    
+    
+    func requestPermissionsButtonTapped() {
+            mapView.location.requestTemporaryFullAccuracyPermissions(withPurposeKey: "CustomKey")
+        }
+    
+    
+}
+extension ViewController: AnnotationInteractionDelegate {
+    public func annotationManager(_ manager: AnnotationManager, didDetectTappedAnnotations annotations: [Annotation]) {
+        print("Annotations tapped: \(annotations)")
+    }
+}
+
+extension ViewController: LocationPermissionsDelegate {
+    
+    func locationManager(_ locationManager: LocationManager, didChangeAccuracyAuthorization accuracyAuthorization: CLAccuracyAuthorization) {
+        if accuracyAuthorization == .reducedAccuracy {
+            // Perform an action in response to the new change in accuracy
+        }
+    }
+    
+    
+}
+
+
+
