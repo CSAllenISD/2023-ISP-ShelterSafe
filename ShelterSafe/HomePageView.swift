f8782cd55b6dc08f2fb84dae22316ac85adb8b10
Shivanshi Das <dasshivanshi@gmail.com>
2023-04-02 18:15:13 -0500

    Sun animation loading

--- ShelterSafe/HomePageView.swift
+++ ShelterSafe/HomePageView.swift
@@ -8,22 +8,44 @@
 import SwiftUI
 
 struct HomePageView: View {
+    @State private var isLoading = false
+    
     var body: some View {
-        ZStack {
-            Color.blue
-                .ignoresSafeArea()
-            
-            VStack {
-                ProgressView()
+        VStack {
+            if isLoading {
+                Image(systemName: "sun.max.fill")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .foregroundColor(.yellow)
                     .padding()
+                    .transition(.scale)
+                    .animation(.easeInOut(duration: 1))
                 
-                Text("Loading...")
-                    .foregroundColor(.white)
+                Text("Welcome to Shelter Safe")
                     .font(.title)
                     .fontWeight(.bold)
+                    .foregroundColor(.white)
+                    .transition(.opacity)
+                    .animation(.easeInOut(duration: 1))
+            } else {
+                ProgressView()
+                    .padding()
+            }
+        }
+        .onAppear {
+            withAnimation {
+                self.isLoading = true
+            }
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                withAnimation {
+                    self.isLoading = false
+                }
             }
         }
     }
 }
+
+
 
 
