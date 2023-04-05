911367fbe1138ba1f16af74e54fa1a25841d60b7
jishnud17 <72703096+jishnud17@users.noreply.github.com>
2023-04-04 22:24:24 -0500

    Changed Files

---
+++ ShelterSafe/HomePageView 2.swift
@@ -0,0 +1,27 @@
+//
+//  HomePageView.swift
+//  ShelterSafe
+//
+//  Created by Zoe Yau on 4/2/23.
+//
+
+import SwiftUI
+
+struct HomePageView: View {
+    var body: some View {
+        Text("Home Page")
+
+    }
+    
+
+
+}
+
+struct HomePageView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomePageView()
+    }
+}
+
+
+
