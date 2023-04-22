3b96a25c0340b858ce384b514b6be732b74d3766
onikh <onik.hoque@student.allenisd.org>
2023-04-22 00:44:01 -0700

    Prototype view that allows users to create their own shelters, and has a button that sends the shelter to our Flask API. In the future, the coordinates should not be manually inputted, but instead taken from a  point on the map that the user taps.

--- ShelterSafe/ShelterListView.swift
+++ ShelterSafe/ShelterListView.swift
@@ -13,34 +13,55 @@ import SwiftUI
 
 struct ShelterListView: View {
     
-   
-    @State var shelters : [Shelter] = []
+    @State private var numberOfShelters : Int = 0
+    @State private var shelterName : String = ""
+    @State private var latitude : Double? = nil
+    @State private var longitude : Double? = nil
     
+    @State private var shelterDescription : String = ""
+    @State private var foodAvailability : String = ""
+    @State private var populationCapacity : String = ""
     
- 
+    //replace this with auto id getter thing
+    
     
     
  
     
     var body: some View {
         VStack {
-            Text("Disaster Details")
-            NavigationView {
-                List {
-                    ForEach(shelters, id: \.id) { shelter in
-                        NavigationLink(destination: Text("test")) { Text(shelter.name)
-                    }
+         Text("Create A Shelter")
+            List {
+                TextField("Shelter Name", text: $shelterName).textFieldStyle(RoundedBorderTextFieldStyle())
+                TextField("Shelter Description", text: $shelterName).textFieldStyle(RoundedBorderTextFieldStyle())
+                TextField("Latitude", value: $latitude, formatter: NumberFormatter())
+                TextField("Longitude", value: $longitude, formatter: NumberFormatter())
+                Picker("Food Availability", selection: $foodAvailability) {
+                    Text("Abundant").tag("Abundant")
+                    Text("Moderate").tag("Moderate")
+                    Text("Shortage").tag("Shortage")
+                }
+                Picker("Population Capacity", selection: $populationCapacity) {
+                    Text("Overfilled").tag("Overfilled")
+                    Text("Near Full").tag("Near Full")
+                    Text("Moderately Filled").tag("Moderately Filled")
+                    Text("Near Empty").tag("Near Empty")
                 }
             }
-
+            Spacer()
+            Button("Create Shelter") {
+                RawData.getShelters { fetchedShelters in
+                    self.numberOfShelters = fetchedShelters.count
                 }
-            .onAppear {
-                    RawData.getShelters() { fetchedUsers in
-                        self.shelters = fetchedUsers
-                    }
-
-        }
-        }
+                
+                let id = self.numberOfShelters + 1
+                let shelter = Shelter(id: id, name: shelterName, latitude: latitude ?? 0, longitude: longitude ?? 0)
+                
+                RawData.addShelter(shelter: shelter)
+                
+            }
+        }.padding()
+ 
         
     }
     
